08af1dd28192374df13ce4caa434fdc27b042b5e
Declan McIntosh <declanmcintosh@gmail.com>
2019-01-23 18:19:10 -0800

    Trying heroku

--- app/main.py
+++ app/main.py
@@ -7,17 +7,24 @@ from api import ping_response, start_response, move_response, end_response
 
 @bottle.route('/')
 def index():
-    return '''
-    Battlesnake documentation can be found at
-       <a href="https://docs.battlesnake.io">https://docs.battlesnake.io</a>.
-    '''
+    data = bottle.request.json
+
+    """
+    TODO: Using the data from the endpoint request object, your
+            snake AI must choose a direction to move in.
+    """
+    print(data)
+    directions = ['up', 'down', 'left', 'right']
+    direction = random.choice(directions)
+
+    return move_response(direction)
+
 
 @bottle.route('/static/<path:path>')
 def static(path):
     """
     Given a path, return the static file located relative
     to the static folder.
-
     This can be used to return the snake head URL in an API response.
     """
     return bottle.static_file(path, root='static/')
@@ -81,6 +88,7 @@ if __name__ == '__main__':
     bottle.run(
         application,
         host=os.getenv('IP', '0.0.0.0'),
-        port=os.getenv('PORT', '80'),
+        port=os.getenv('PORT', '8080'),
         debug=os.getenv('DEBUG', False)
     )
+
