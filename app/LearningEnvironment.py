e912fe54e14399958f040d1af0680c44cf51384b
Robert Lee <32447910+robertklee@users.noreply.github.com>
2019-01-30 19:56:47 -0800

    added code for head encoding, parsing of enemy snake body segment location into array

--- app/LearningEnvironment.py
+++ app/LearningEnvironment.py
@@ -8,9 +8,10 @@ from gym import spaces
 from gym.utils import seeding
 
 max_board_size = 21
+max_health = 100
 
 class Snekgame(gym.Env):
-    '''Snek environement for snek game snek
+    '''Snek environment for snek game snek
     '''
     def __init__(self):
 
@@ -23,14 +24,16 @@ class Snekgame(gym.Env):
         self.init_wholesome_boi()
 
         ## Board Encoding defintion 
-        self.noGo = 1.0
-        self.empty = 0
-        self.food = -0.25
-        self.ourHead = -1
-        self.bodyNorth = 0.7
-        self.bodySouth = 0.6
-        self.bodyEast = 0.5
-        self.bodyWest = 0.4
+        self.noGo           = 1.0
+        self.empty          = 0
+        self.food           = -0.25
+        self.ourHead        = -1
+        self.bodyNorth      = 0.7
+        self.bodySouth      = 0.6
+        self.bodyEast       = 0.5
+        self.bodyWest       = 0.4
+        self.headZeroHP     = 0.8 # 0.8 <= head <= 0.9
+        self.headMaxHP      = 0.9 # 0HP --------> max_health
         ## Board Encoding definition
 
         self.boundsUpper = 1
@@ -41,7 +44,7 @@ class Snekgame(gym.Env):
         self.newJsonDataFlag = False
 
         #Previous state variables
-        self.previousHP = 10000000      #used to determine whether snake has eaten
+        self.previousHP = max_health + 1      #used to determine whether snake has eaten
         self.previousNumSnakes = -1     #used to determine whether other snake has died
         self.previousReward = 0
 
@@ -76,6 +79,8 @@ class Snekgame(gym.Env):
 
         currentHP = data["you"]["health"]
 
+        currentLength = len(data["you"]["body"])
+
         # if currentHP has increased, snake must have eaten
         if (currentHP > self.previousHP):
             reward += self.eatReward
@@ -85,15 +90,72 @@ class Snekgame(gym.Env):
         if (numSnakesAlive < self.previousNumSnakes):
             reward += self.killReward
         
-        #TODO do the stuff here after we have new data
         board_state = np.zeros((max_board_size, max_board_size)) # numpy array of size we defined for self.observation_space 
 
-        # Fill empty spaces with walls
-        for row in range(board["height"], max_board_size):
+        # Fill wall locations
+        for row in range(0, board["height"]):
             for col in range(board["width"], max_board_size):
                 board_state[row,col] = self.noGo
-        
 
+        for row in range(board["height"], max_board_size):
+            for col in range(0, max_board_size):
+                board_state[row,col] = self.noGo
+
+        # Fill enemy snake body locations
+        for enemy_snake in board["snakes"]:
+            # grab each enemy snake (by the head)
+
+            enemy_health = enemy_snake["health"]
+            enemy_length = len(enemy_snake["body"])
+
+            # calculate hp value to encode in head position
+            enemy_head_val = enemy_health / max_health * (self.headMaxHP-self.headZeroHP) + self.headZeroHP
+
+            if (enemy_length < currentLength):
+                enemy_head_val *= -1
+            
+            # get head of enemy snake
+            enemy_head_location = enemy_snake["body"][0]
+            enemy_body_prev_x = enemy_head_location["x"]
+            enemy_body_prev_y = enemy_head_location["y"]
+
+            # encode hp value into head position
+            board_state[enemy_body_prev_x, enemy_body_prev_y] = enemy_head_val
+
+            # encode rest of body into board, where each body segment points to 
+            # the direction of the previous body segment
+            for i in range(1, len(enemy_snake["body"])):
+                enemy_body_x = enemy_snake["body"][i]["x"]
+                enemy_body_y = enemy_snake["body"][i]["y"]
+
+                # keep in mind, x is used to index the row number (increasing x means moving south), 
+                # and y is used to index column number (increasing y means moving east)
+                #  y-> 0 1 ... n
+                # x:  ___________
+                # 0   |         |
+                # 1   |         |
+                # ... |         |
+                # n   |         |
+                #     -----------
+                if (enemy_body_x == enemy_body_prev_x):
+                    # x value unchanged, so body segment must differ in y
+                    board_state[enemy_body_x, enemy_body_y] = \
+                        self.bodyWest if (enemy_body_y > enemy_body_prev_y) else self.bodyEast
+                else:
+                    # body segment must differ in x
+                    board_state[enemy_body_x,enemy_body_y] = \
+                        self.bodyNorth if (enemy_body_x > enemy_body_prev_x) else self.bodySouth
+
+                enemy_body_prev_x = enemy_body_x
+                enemy_body_prev_y = enemy_body_y
+
+
+        # Fill food locations
+        for xy_pair in board["food"]:
+            if (board_state[xy_pair["x"], xy_pair["y"]] == 0):
+                board_state[xy_pair["x"], xy_pair["y"]] = self.food
+            # else:
+            #     raise ValueError('Food occurred in place where something already exists!')
 
         observation = "TODO" # flatten board_state matrix, and add HP flag
 
