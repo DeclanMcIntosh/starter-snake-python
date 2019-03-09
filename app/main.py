6098b3bab7571415e4c0bb39517a7ddf6319f65d
delcanmcintosh <declanmcintosh@gmail.com>
2019-03-09 13:58:49 -0800

    Fixed up main.py

--- app/main.py
+++ app/main.py
@@ -1,19 +1,20 @@
+# Designed for ofline (local) self-play with a training dummy,
+
 import json
 import os
 import random
 import bottle
 import threading
 import time
-from LearningEnvironment2 import *
+from LearningEnvironment import *
 from LearningMain import *
 
 from api import ping_response, start_response, move_response, end_response
 
 #Flags for what kind of network we are training
-sizeType = 19
+boardSizeType = 19
 
-
-envi = Snekgame(max_board_size=sizeType)
+envi = Snekgame(max_board_size=boardSizeType)
 envi.init_just_win_aggresive()
 
 
@@ -23,10 +24,12 @@ def index():
 
     Battlesnake documentation can be found at
 
-       <a href="https://docs.battlesnake.io">https://docs.battlesnake.io</a>. This snake is ##SNAKE_PERSONALITY_INSERT_HERE##, its created with reinforcement learning algorithms powered by tensorflow, keras and keras-rl. :3
+       <a href="https://docs.battlesnake.io">https://docs.battlesnake.io</a>. 
+       This snake is Kevin. 
+       It's created with reinforcement learning algorithms powered by tensorflow, keras and keras-rl.
+       :3
 
     '''
-
 
 @bottle.route('/static/<path:path>')
 def static(path):
@@ -43,30 +46,26 @@ def ping():
 
 @bottle.post('/start')
 def start():
-    #print("start Request recived")
-    data = bottle.request.json
     color = "#00FF00"
-    #envi.sendNewData(data)
-    #print("start Request responded")
     return start_response(color)
 
 
 @bottle.post('/move')
 def move():
-    #print("move Request recived")
     global envi
     data = bottle.request.json
     if data != None:
+        # Send new data to envrionment
         envi.sendNewData(data)
         move = None
-        counter = 0 
-        while move == None and counter < 9000:
+        noMoveTimeoutCounter = 0 
+        # Wait for network to generate move or timeout.
+        while move == None and noMoveTimeoutCounter < 9000:
             move = envi.getMove()
             time.sleep(0.0001)
-            counter += 1
+            noMoveTimeoutCounter += 1
         if move == None:
             move = "left"
-        #print("move Request responded")
         return move_response(move)
     return move_response("left")
 
@@ -74,30 +73,32 @@ def move():
 @bottle.post('/end')
 def end():
     data = bottle.request.json
-    won = False
-    #if len(data["board"]["snakes"]) == 1 and (data["board"]["snakes"][0]["name"] == "legless lizzard" or data["board"]["snakes"][0]["name"] == "0"):
+    wonGame = False
     snakeNames = []
     for snake in data["board"]["snakes"]:
         snakeNames.append(snake["name"])
-    if len(data["board"]["snakes"]) == 1 and ("legless lizzard" in snakeNames or "0" in snakeNames or data["you"]["name"] in snakeNames):
-        won = True
-    envi.endEnvi(won)
-    #envi.sendNewData(data)
-    #print("end Request responded")
+    # Check if we are in the list of snakes at the termination of the game, if so
+    # we have won!
+    if len(data["board"]["snakes"]) == 1 and ("legless lizzard" in snakeNames \
+        or "0" in snakeNames or data["you"]["name"] in snakeNames):
+        wonGame = True
+    envi.endEnvi(wonGame)
     return end_response()
 
 # Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app()
 
 if __name__ == '__main__':
+    # Create server thread
     threading.Thread(target=bottle.run, kwargs=dict(
         app=application,
         host=os.getenv('IP', '0.0.0.0'),
-        port=os.getenv('PORT', '81'),
+        port=os.getenv('PORT', 'You would like that wouldent you'), # You need to supply a port forwarded port.
         debug=os.getenv('DEBUG', False),
         quiet=True
         )
     ).start()
+    # Create keras-rl management thread
     threading.Thread(target=startLearning, kwargs=dict(
-        Env=envi, max_board_size=sizeType, loadFileNumber=23)
+        Env=envi, max_board_size=boardSizeType, loadFileNumber=23)
     ).start()
